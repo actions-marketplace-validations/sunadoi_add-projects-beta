5202e1ffbff9709d773be9dd0aaffe1d3f444147
sunadoi <y.sunadoi@gmail.com>
2022-02-21 21:47:05 +0900

    fix: core

--- index.ts
+++ index.ts
@@ -1,15 +1,24 @@
-import core from "@actions/core";
-import { graphql, GraphQlQueryResponseData } from "@octokit/graphql";
+import { getInput } from "@actions/core";
+import {
+  graphql,
+  GraphQlQueryResponseData,
+  GraphqlResponseError,
+} from "@octokit/graphql";
 
 const main = async () => {
   const graphqlWithAuth = graphql.defaults({
     headers: {
-      authorization: core.getInput("github-token"),
+      authorization: `token ${getInput("github-token")}`,
     },
   });
 
   const query = `
     query($login: String!, $projectNumber: Int!) {
+      user(login: $login) {
+        projectNext(number: $projectNumber) {
+          id
+        }
+      }
       organization(login: $login) {
         projectNext(number: $projectNumber) {
           id
@@ -30,14 +39,16 @@ const main = async () => {
 
   try {
     const queryResult: GraphQlQueryResponseData = await graphqlWithAuth(query, {
-      login: core.getInput("project-owner"),
-      projectNumber: Number(core.getInput("project-number")),
+      login: getInput("project-owner"),
+      projectNumber: Number(getInput("project-number")),
     });
 
     const projectNodeId =
       queryResult?.user?.projectNext?.id ||
       queryResult?.organization?.projectNext?.id ||
       "";
+
+    console.log(projectNodeId);
     if (!projectNodeId) {
       console.error(`no project node id`);
     }
@@ -46,12 +57,16 @@ const main = async () => {
       mutation,
       {
         projectId: projectNodeId,
-        contentId: core.getInput("content-id"),
+        contentId: getInput("content-id"),
       }
     );
     return mutationResult?.addProjectNextItem?.projectNextItem?.id || "";
   } catch (error: unknown) {
-    if (error instanceof Error) console.error(error.message);
+    console.log("error");
+    if (error instanceof GraphqlResponseError) {
+      console.error("Request failed: ", error.request);
+      console.error(error.message);
+    }
     return;
   }
 };
